0f114b8d70e09ec9546fcf85713232515ed3cb8b
Sandwich2080 <androidsandwich@163.com>
2020-01-13 14:45:19 +0800

    [modified] change code segments to method.

--- com/demo/pythondemo/requests/requests_demo.py
+++ com/demo/pythondemo/requests/requests_demo.py
@@ -9,21 +9,31 @@ def print_log(str_content):
     print(str_content)
 
 
-request_url = 'https://api.github.com/repos/psf/requests'
-print_log('request_url:' + request_url + '\n')
-r = requests.get(request_url)
+def http_get_test():
+    r""" http get test
 
-# print headers
-headers = r.headers
-for key in headers:
-    print_log(key + ':' + headers[key])
-print_log('\n')
+    :return:
+    """
 
-# print body content
-json_response = r.json()
-print_log(json_response)
+    request_url = 'https://api.github.com/repos/psf/requests'
+    print_log('request_url:' + request_url + '\n')
+    r = requests.get(request_url)
 
-# print(headers)
-# print('Date:'+headers['Date'])
-# print('Content-Type:'+headers['Content-Type'])
-# print('Transfer-Encoding:'+headers['Transfer-Encoding'])
+    # print headers
+    headers = r.headers
+    for key in headers:
+        print_log(key + ':' + headers[key])
+    print_log('\n')
+
+    # print body content
+    json_response = r.json()
+    print_log(json_response)
+
+    # print(headers)
+    # print('Date:'+headers['Date'])
+    # print('Content-Type:'+headers['Content-Type'])
+    # print('Transfer-Encoding:'+headers['Transfer-Encoding'])
+
+
+# Http test
+http_get_test()
