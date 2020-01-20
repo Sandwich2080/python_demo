f8973e7c5f5f3434f8fbf46695fe752ce5e295ec
Sandwich2080 <androidsandwich@163.com>
2020-01-20 15:24:33 +0800

    [modified] Add some comments.

--- com/demo/pythondemo/task/scheduler_task.py
+++ com/demo/pythondemo/task/scheduler_task.py
@@ -2,8 +2,14 @@ import time
 
 
 def timer(n, your_callback_method):
+    r"""
+    :param n: The duration to execute the task (in seconds)
+    :param your_callback_method: The task method to execute
+    :return:
+    """
+
     while True:
         current_time = time.strftime('%Y-%m-%d %X', time.localtime())
-        print('self:'+current_time)
+        print('self:' + current_time)
         your_callback_method(current_time)
         time.sleep(n)
